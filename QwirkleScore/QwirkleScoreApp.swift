914849bd3bf8e7414a81834b32a2a1c489d2e2f5
YCLin mbp <a50206123@gmail.com>
2024-07-16 00:28:53 +0800

    Initial Commit

---
+++ QwirkleScore/QwirkleScoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  QwirkleScoreApp.swift
+//  QwirkleScore
+//
+//  Created by Y.C Lin on 2024/07/16.
+//
+
+import SwiftUI
+
+@main
+struct QwirkleScoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
